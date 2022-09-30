e71922922c74b5992da78f9e48891a717a9742b4
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-09-30 19:06:48 +0900

    CSR(Client Side Render)
    클라이언트가 데이터를 가져와서 그린다

--- pages/csr.js
+++ pages/csr.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
 
-export async function getServerSideProps() {
-  console.log("server");
-  return {
-    props: { time: new Date().toISOString() },
-  };
-}
+export default function CSR() {
+  const [time, setTime] = useState();
 
-export default function Home({ time }) {
+  useEffect(() => {
+    console.log("client");
+    setTime(new Date().toISOString());
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,11 +21,6 @@ export default function Home({ time }) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>{time}</h1>
-        <h1>
-          <Link href="/csr">
-            <a>CSR 로</a>
-          </Link>
-        </h1>
       </main>
 
       <footer className={styles.footer}>
